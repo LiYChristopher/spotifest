d0684e2af1370f7ac77f74ca1c7a85eb3531d371
Christopher Li <LiYChristopher92@gmail.com>
2016-01-24 17:21:50 -0500

    Completed OAuth flow into app (at least first version of it)

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 #config.py
 
+
 class BaseConfig(object):
-    CLIENT_ID = 'ddcd27ddd412424697d8af8cf3a5bc17'
-    CLIENT_SECRET = 'adc6a0f42ece438798e3e29aad9cff95'
-    REDIRECT_URI = 'http://www.google.com/'
+
+	with open('credentials.txt', 'r') as cred:
+		CLIENT_ID = str(cred.readline().split('>')[1].replace('\n', ''))
+		CLIENT_SECRET = str(cred.readline().split('>')[1].replace('\n', ''))
+		REDIRECT_URI = str(cred.readline().split('>')[1].replace('\n', ''))
+
+	if not CLIENT_ID or not CLIENT_SECRET or not REDIRECT_URI:
+		raise Exception('Credentials could not be configured. See credentials.txt.')
