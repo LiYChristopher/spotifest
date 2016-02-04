d6f5b31f9263b18de0bf94dbce5722dfb794a65d
Andres Hazard <andres.hazard@servicerocket.com>
2016-02-04 12:35:06 -0300

    small correction on db script

--- mysqlScript.sql
+++ mysqlScript.sql
@@ -3,7 +3,7 @@ use festify;
 CREATE TABLE sessions (
     festivalId int(10) NOT NULL AUTO_INCREMENT,
     festivalName varchar(100) NOT NULL,
-    userID varchar(30) NOT NULL,
+    userId varchar(30) NOT NULL,
     playlistId varchar(30) NOT NULL,
     playlistURL varchar(512) NOT NULL,
     catalogId varchar(30) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE sessions (
 );
 CREATE table contributors (
     festivalId int (10) NOT NULL,
-    userID varchar(30) NOT NULL,
+    userId varchar(30) NOT NULL,
     CONSTRAINT FOREIGN KEY (festivalId) REFERENCES sessions(festivalId)
 );
 
