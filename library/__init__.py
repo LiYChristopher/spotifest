0a43889f8c97f40dadb354e5b4a9c3e031d95765
Christopher Li <LiYChristopher92@gmail.com>
2016-02-14 16:17:32 -0500

    Method works on local too, passing back minor changes to be tested on DO

--- library/__init__.py
+++ library/__init__.py
@@ -5,23 +5,21 @@ from flask.ext.login import LoginManager
 from flask.ext.mysql import MySQL
 import os
 
+
 def create_app(config=None, app_name=None, blueprints=None):
     app = Flask(__name__)
     return app
 
-def find_config():
-    # because we know for our app that config is one layer
-    # outside of library
-    config_fname = 'config.py'
+
+def find_config(filename):
+    config_fname = filename
     app_root = os.getcwd().split('library')[0]
-    # it's ugly, but it's all we need.
-    # time to locate config.py
     for r, d, f in os.walk(app_root):
         if config_fname in f:
             return os.path.join(app_root, config_fname)
     raise IOError("Config file name at '{}' could not be found.".format(config_fname))
 
-path = find_config()
+path = find_config('config.py')
 os.environ['APP_CONFIG'] = path
 
 app = create_app()
@@ -30,14 +28,10 @@ app.config.from_envvar('APP_CONFIG')
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 mysql = MySQL()
-
-# mysql configurations
-
 
 mysql.init_app(app)
 
